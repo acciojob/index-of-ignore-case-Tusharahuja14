6d9e846a64f8a20119a8ab60d428771e2acce1a0
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-01-07 16:35:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	s1=s1.toLowerCase();
+	s2=s2.toLowerCase();
+	let j=0;
+	let st=-1;
+	for(let i=0;i<s1.length)
+		{
+			if(s1.charAt(i)==s2.charAt(j))
+			{
+				st=i;
+				j++;
+			}
+			else{
+				j=0;
+			}
+			
+		}
+	return st;
+	
 }
 
 // Please do not change the code below
